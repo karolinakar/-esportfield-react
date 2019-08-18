fd8af04c85d3eb4ed934b0f2790097159fd09313
karolinaakarczewskaa <karolinaakarczewskaa@gmail.com>
2019-08-18 20:08:19 +0200

    Updates

---
+++ precache-manifest.a8ed54360ecb0b02c1677a7fefd9a9fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df0e3efe0225e63f49698ba0bccf51c",
+    "url": "/esportfield-react/index.html"
+  },
+  {
+    "revision": "5ba6e4981ebf3c7dc0c3",
+    "url": "/esportfield-react/static/css/2.bfe7958e.chunk.css"
+  },
+  {
+    "revision": "db717a495961755ea96c",
+    "url": "/esportfield-react/static/css/main.29b9c096.chunk.css"
+  },
+  {
+    "revision": "5ba6e4981ebf3c7dc0c3",
+    "url": "/esportfield-react/static/js/2.7a9859d6.chunk.js"
+  },
+  {
+    "revision": "db717a495961755ea96c",
+    "url": "/esportfield-react/static/js/main.38865ee3.chunk.js"
+  },
+  {
+    "revision": "85c7a17a1b02914b048e",
+    "url": "/esportfield-react/static/js/runtime~main.dc3a449e.js"
+  }
+]);
